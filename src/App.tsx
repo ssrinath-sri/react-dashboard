b45ac78e78ca435aa56db85515bbc76db9535c05
ssrinath-sri <ssrinath.sri@gmail.com>
2024-01-24 13:13:09 +0530

    added the cards and tables react components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import { FC } from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
 
 
 import './style.css';
@@ -7,9 +16,96 @@ import './style.css';
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
-      <h1>Hello {name}!</h1>
-      <p>Start cliking on below link to see some magic happen :)</p>
-      <Button variant="secondary">Dash board</Button>{' '}
-    </div>
+
+    {/* header */}
+
+     <Navbar className="bg-body-tertiary">
+     <Container fluid>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="./assets/robot.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Dashboard
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      {/* Main-Content over here */}
+
+      <Container>
+      <Row>
+        <Col><Card style={{ width: '18rem' }}>
+      <Card.Header>Featured</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </Card></Col>
+        <Col><Card style={{ width: '18rem' }}>
+      <Card.Header>Featured</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </Card></Col>
+        <Col><Card style={{ width: '18rem' }}>
+      <Card.Header>Featured</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </Card></Col>
+       
+      </Row>
+    </Container>
+
+    {/* table */}
+    <Container>
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+    </Container>
+   
+      {/* footer */}
+
+      <Navbar className="bg-body-tertiary">
+      <Container fluid>
+        <p>Copy right @2024 </p>
+        </Container>
+      </Navbar>
+          </div>
   );
 };
---
+++ src/layout/layout.tsx
@@ -0,0 +1,5 @@
+// import {} from '../components/top-header';
+// import {} from '../components/side-nav';
+// import {} from '../components/footer/';
+
+// export Layout
